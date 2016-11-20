3d5b7b2523b6bb00962fe89c3a901030775ce90d
Yunzhe@ZJU <iLoveLive@honoka.moe>
2016-11-20 18:04:04 +0800

    now we can set up and run the sever with few functions

---
+++ test/config.py
@@ -0,0 +1,27 @@
+"""can add in your own MySQL config and comment out others',
+ no need to change the database connection code in this case"""
+
+# config for Yujia- MySQL
+# USERNAME = "root"
+# PASSWORD = "jiu bu gao su ni" #shuo de hao xiang ni gao su wo wo jiu neng connect to your local host shi de.
+# SCHEMA = "mybookstore"
+
+# config for Siyuan- MySQL
+# USERNAME =
+# PASSWORD =
+# SCHEMA =
+
+# config for Jiuqi- MySQL
+# USERNAME = "root"
+# PASSWORD = ""
+# SCHEMA = "mybookstore"
+
+# config for Xianyue- MySQL
+# USERNAME =
+# PASSWORD =
+# SCHEMA =
+
+# config for Yunzhe- MySQL
+USERNAME = "testuser"
+PASSWORD = "test623"
+SCHEMA = "mybookstore"

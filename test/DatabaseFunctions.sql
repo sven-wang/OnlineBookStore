1cba43a591aa6918b7180b00349d7dbec2057ea2
jorch <jiuqi_zhou@mymail.sutd.edu.sg>
2016-12-09 23:14:10 +0800

    Database Functions Update

--- test/DatabaseFunctions.sql
+++ test/DatabaseFunctions.sql
@@ -1,7 +1,6 @@
-##Jiuqi
 CREATE TABLE Books(
     ISBN CHAR(13),
-    title CHAR(100),
+    title CHAR(255),
     authors VARCHAR(256),
     publisher VARCHAR(64),
     year INTEGER,
@@ -12,67 +11,99 @@ CREATE TABLE Books(
     subject VARCHAR(100),
     PRIMARY KEY(ISBN)
 );
-    
 
-#xy
 CREATE TABLE Customers(
-    login_name CHAR(50) primary key,
+    login_name CHAR(50),
     full_name CHAR(50),
     passwords CHAR(20),
-    card_num CHAR(20),
+    card_num CHAR(20) UNIQUE,
     address CHAR(50),
-    phone_num CHAR(20)
+    phone_num CHAR(20),
+    PRIMARY KEY(login_name)
 );
 
-## Yujia
-CREATE TABLE Orders( 
-    login_name CHAR(50),
-    ISBN CHAR(13),
-    copies INTEGER,
-    dateTime DATETIME,
-    status CHAR(20),
-    PRIMARY KEY (login_name, ISBN, dateTime),
-    FOREIGN KEY (login_name) REFERENCES Customers,
-    FOREIGN KEY (ISBN) REFERENCES Books
-    );
-
-
-## siyuan 
 CREATE TABLE Feedbacks(
-    fid INTEGER PRIMARY KEY,
     login_name CHAR(50),
     ISBN CHAR(13),
     dateTime DATETIME,
     score INTEGER CHECK(score >=1 AND score <= 10),
     text CHAR(200),
-    FOREIGN KEY (login_name) REFERENCES Customers,
-    FOREIGN KEY (ISBN) REFERENCES Books
+    PRIMARY KEY (login_name, ISBN),
+    FOREIGN KEY (login_name) REFERENCES Customers(login_name) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (ISBN) REFERENCES Books(ISBN) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+CREATE TABLE Orders(
+    oid INTEGER,
+    login_name CHAR(50),
+    date DATE,
+    status CHAR(20),
+    PRIMARY KEY (oid),
+    FOREIGN KEY (login_name) REFERENCES Customers(login_name) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
-##yunzhe
+CREATE TABLE Items(
+	oid INTEGER,
+    ISBN CHAR(13),
+    copies INTEGER,
+    PRIMARY KEY (oid, ISBN),
+    FOREIGN KEY (ISBN) REFERENCES Books(ISBN) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (oid) REFERENCES Orders(oid) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
 CREATE TABLE Rate(
-    login_name CHAR(50), 
-    fid INTEGER,
+    rater_name CHAR(50),
+    feedback_name CHAR(50),
+    ISBN CHAR(13),
     usefulness INTEGER CHECK (usefulness = 0 OR usefulness = 1 OR usefulness = 2),
-    PRIMARY KEY (login_name, fid),
-    FOREIGN KEY (login_name) REFERENCES Customers,
-    FOREIGN KEY (fid) REFERENCES Feedbacks,
-    CHECK login_name IN (
-        SELECT)login_name
-        FROM (
-            SELECT 
-        WHERE 
-    
+    PRIMARY KEY (rater_name, feedback_name, ISBN),
+    FOREIGN KEY (rater_name) REFERENCES Customers(login_name) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (feedback_name) REFERENCES Feedbacks(login_name) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (ISBN) REFERENCES Feedbacks(ISBN) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+CREATE TABLE Managers(
+    login_name CHAR(50),
+    full_name CHAR(50),
+    passwords CHAR(20),
+    address CHAR(50),
+    phone_num CHAR(20),
+    PRIMARY KEY(login_name)
+);
+
 
 
 ## 1) Registration
-# Yujia
-INSERT INTO Customers (login_name, full_name, passwords, card_num, address, phone_num) 
+INSERT INTO Customers
 VALUES (login_name, full_name, passwords, card_num, address, phone_num);
 
 ## 2) Ordering
+# Ordering Function
+#check most recent order status
+SELECT status
+FROM (SELECT login_name, status, oid
+	  FROM Orders
+  	  WHERE login_name = "login_name")
+WHERE oid = MAX(oid)
+
+# if status == 'Completed'
+  # get max oid
+  SELECT MAX(oid)
+  FROM Orders
+  # allocate new oid & get current date
+  oid = oid + 1
+  # insert a new order
+  INSERT INTO Orders(oid, "login_name", date, "Processing")
+  # insert a new item
+  INSERT INTO Items(oid, "ISBN", copies)
+
+# if status == 'Processing'
+  # get current oid
+  SELECT MAX(oid)
+  FROM Orders
+  WHERE login_name = "login_name"
+  # insert new item under current oid
+  INSERT INTO Items(oid, "ISBN", copies)
 
 #ViewCart Function
 SELECT Books.ISBN, title, authors, publisher, year, copies, price, format, subject, copies
@@ -85,14 +116,13 @@ WHERE info.ISBN = Books.ISBN
 
 
 ## 3) User record
-#Jiuqi
-# his/her account information 
+# his/her account information
 SELECT *
 FROM Customers
 WHERE login_name = "login_name"
 
 # his/her full history of orders
-SELECT * 
+SELECT *
 FROM Orders
 WHERE login_name = "login_name"
 
@@ -107,50 +137,40 @@ FROM Feedback f, Rate r
 WHERE r.login_name = "login_name" AND f.fid = r.fid
 
 ## 4) New book
-#xy
-INSERT INTO Books (ISBN, title, authors, publisher, year, copies, price, format, keywords, subject)
+INSERT INTO Books
 VALUES (ISBN, title, authors, publisher, year, copies, price, format, keywords, subject);
 
 
 ## 5) Arrival of more copies
-#yunzhe
 UPDATE Books
-SET copies = 
-WHERE ISBN = 
+SET copies = copies + (new copies)
+WHERE ISBN = "ISBN"
 
 ## 6) Feedback recordings
-# Yujia
-INSERT INTO Feedbacks (fid, cid, bid, dateTime, score, text) 
-VALUES (fid, cid, bid, dateTime, score, text);
+INSERT INTO Feedbacks
+VALUES (login_name, ISBN, date, score, text) ;
 
-INSERT INTO Rate VALUES ('Nobody', '+ feedback_name +', '+ISBN+', 0 )
+INSERT INTO Rate
+VALUES ('Nobody', feedback_name, ISBN, 0)
 
 ## 7) Usefulness ratings
-INSERT INTO Feedbacks VALUES ('"+rater_name+"', '"+feedback_name+"', '"+ISBN+"', '"+usefulness+"');"
-
-
-
-
+INSERT INTO Feedbacks
+VALUES (rater_name, feedback_name, ISBN, usefulness);
 
 ## 8) Book Browsing
-# siyuan
-# conjunctive query
-SELECT * FROM Books 
-WHERE LOWER(authors) LIKE LOWER("%authorName%") 
-AND LOWER(publisher) = LOWER("publisher")
-AND LOWER(title) = LOWER("%title%")
-AND LOWER(subject) = LOWER("subject")
-# sorting 
+SELECT * FROM Books
+WHERE LOWER(authors) LIKE LOWER("%author%") AND
+      LOWER(publisher) LIKE LOWER("%publisher%") AND
+      LOWER(title) LIKE LOWER("%title%") AND
+      LOWER(subject) LIKE LOWER("%subject%")
 
 ## 9) Useful feedbacks
-# siyuan
 SELECT Rate.feedback_name, Feedbacks.text, Feedbacks.score, AVG(usefulness), Feedbacks.date FROM Rate, Feedbacks
 WHERE Rate.feedback_name = Feedbacks.login_name AND Feedbacks.ISBN = Rate.ISBN AND Rate.ISBN = '9781449389673'
 GROUP BY Rate.feedback_name ORDER BY AVG(Rate.usefulness) DESC LIMIT 5;
 
 
 ## 10) Book recommendation
-## siyuan
 SELECT ISBN, sum(copies)
 FROM Orders, Items
 WHERE Orders.oid = Items.oid
@@ -168,35 +188,37 @@ AND Orders.login_name in (SELECT distinct login_name
 GROUP BY ISBN
 ORDER BY sum(copies) DESC
 
-## 11) Statistics 
+## 11) Statistics
 
+#the list of the m most popular books
 SELECT ISBN, SUM(copies)
 FROM (SELECT ISBN, copies
-	  FROM Orders JOIN Items on Orders.oid = Items.oid
-	  WHERE  MONTH(date) = 12 AND YEAR(date) = 2016 AND status = 'Complete') info
+	    FROM Orders o, Items i
+	    WHERE o.oid = i.oid AND MONTH(date) = "month" AND YEAR(date) ="year" AND status = 'Complete') info
 GROUP BY ISBN
 ORDER BY SUM(copies) DESC
-LIMIT 1
+LIMIT m
 
+#the list of m most popular authors
 SELECT authors, SUM(sale)
 FROM (SELECT ISBN, SUM(copies) sale
-	  FROM (SELECT ISBN, copies
-	  		FROM Orders o, Items i
-	  		WHERE o.oid = i.oid AND MONTH(date) = 12 AND YEAR(date) = 2016 AND status = 'Complete') info
-	  GROUP BY ISBN) sales, Books
+	    FROM (SELECT ISBN, copies
+	  		    FROM Orders o, Items i
+	  		    WHERE o.oid = i.oid AND MONTH(date) = "month" AND YEAR(date) = "year" AND status = 'Complete') info
+	    GROUP BY ISBN) sales, Books
 WHERE sales.ISBN = Books.ISBN
 GROUP BY authors
 ORDER BY SUM(sale) DESC
-LIMIT 1
+LIMIT m
 
-
+#the list of m most popular publisher
 SELECT publisher, SUM(sale)
 FROM (SELECT ISBN, SUM(copies) sale
-	  FROM (SELECT ISBN, copies
-	  		FROM Orders o, Items i
-	  		WHERE o.oid = i.oid AND MONTH(date) = 12 AND YEAR(date) = 2016 AND status = 'Complete') info
-	  GROUP BY ISBN) sales, Books
+	    FROM (SELECT *
+	  		    FROM Orders o, Items i
+	  		    WHERE o.oid = i.oid AND MONTH(date) = "month" AND YEAR(date) = "year" AND status = 'Complete') info
+	    GROUP BY ISBN) sales, Books
 WHERE sales.ISBN = Books.ISBN
 GROUP BY publisher
 ORDER BY SUM(sale) DESC
-LIMIT 1
+LIMIT m

05df5fb9b55bc8b41170e40556237bfba6603121
Sven-Siyuan-Wang <siyuanwang1123@gmail.com>
2016-12-09 23:11:06 +0800

    book browsing ORDER function finished

--- test/DatabaseFunctions.sql
+++ test/DatabaseFunctions.sql
@@ -125,21 +125,24 @@ VALUES (fid, cid, bid, dateTime, score, text);
 INSERT INTO Rate VALUES ('Nobody', '+ feedback_name +', '+ISBN+', 0 )
 
 ## 7) Usefulness ratings
-INSERT INTO Feedbacks VALUES ('"+rater_name+"', '"+feedback_name+"', '"+ISBN+"', '"+usefulness+"');"
+INSERT INTO Feedbacks VALUES ('"+rater_name+"', '"+feedback_name+"', '"+ISBN+"', '"+usefulness+"');
 
 
 
 
 
 ## 8) Book Browsing
-# siyuan
-# conjunctive query
-SELECT * FROM Books 
-WHERE LOWER(authors) LIKE LOWER("%authorName%") 
-AND LOWER(publisher) = LOWER("publisher")
-AND LOWER(title) = LOWER("%title%")
-AND LOWER(subject) = LOWER("subject")
-# sorting 
+## conjunctive query with order options
+
+
+SELECT Books.ISBN, title, authors, publisher, year, copies, price, format,keywords, subject, AVG(score) FROM Books natural join Feedbacks
+WHERE LOWER(authors) LIKE LOWER("%authorName%")
+AND LOWER(publisher) LIKE LOWER("%publisher%")
+AND LOWER(title) LIKE LOWER("%title%")
+AND LOWER(subject) LIKE LOWER("%subject%")
+GROUP BY Books.ISBN
+#ORDER BY year DESC
+ORDER BY AVG(score)
 
 ## 9) Useful feedbacks
 # siyuan

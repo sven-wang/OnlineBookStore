ed4c687a62c93e86dfa46ac2518644b38041c5f3
Sven-Siyuan-Wang <siyuanwang1123@gmail.com>
2016-12-10 23:58:32 +0800

    fixed viewCart bug

--- test/dbOpTest.py
+++ test/dbOpTest.py
@@ -4,7 +4,7 @@ authors = "author"
 publisher = "pub"
 title = "jiuqi s life"
 subject = "sub"
-login_name = "1"
+login_name = "firstUser"
 ISBN = "ISBN"
 dateTime = "12-12-1996"
 score = "4"
@@ -22,26 +22,10 @@ oid = '20160003'
 sortBy = 'year'
 Desc_Asc = 'd'
 
-query = "SELECT Items.ISBN, Books.title, sum(Items.copies) \
-                FROM Orders, Items, Books \
-                WHERE Orders.oid = Items.oid \
-                AND Items.ISBN in (SELECT distinct ISBN \
-                             FROM (SELECT distinct login_name \
-                                    FROM Orders, Items \
-                                    WHERE Orders.oid = Items.oid  \
-                                    AND ISBN = '" + ISBN + "') C, Orders O, Items I \
-                               WHERE c.login_name = o.login_name \
-                                AND O.oid = I.oid) \
-                AND Orders.login_name in (SELECT distinct login_name \
-                                            FROM Orders, Items \
-                                            WHERE Orders.oid = Items.oid  \
-                                            AND ISBN = '" + ISBN + "') \
-                AND Items.ISBN not in (SELECT  ISBN   \
-	             			FROM Orders, Items  \
-	               			WHERE Orders.oid = Items.oid    \
-	                    	AND login_name = '" + login_name + "') \
-                AND Items.ISBN = Books.ISBN \
-                GROUP BY Items.ISBN \
-                ORDER BY sum(copies) DESC"
+query = "SELECT oid, date, title, info.copies \
+                    FROM (SELECT oid, date, ISBN, copies \
+                          FROM Orders o NATURAL JOIN Items i \
+                          WHERE login_name = " + login_name + ") info, Books \
+                    WHERE info.ISBN = Books.ISBN "
 
 print query
